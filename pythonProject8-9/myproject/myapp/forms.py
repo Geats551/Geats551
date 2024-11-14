3cce63b7e65e97dfa679e705eaf01013176deeb3
yuwuhuwu <1063541164@qq.com>
2024-11-14 15:36:12 +0800

    xcvb

--- pythonProject8-9/myproject/myapp/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-
-from django import forms
-from .models import AgriculturalProduct
-
-class AgriculturalProductForm(forms.ModelForm):
-    class Meta:
-        model = AgriculturalProduct
-        fields = ['name', 'description', 'image', 'price', 'category']
