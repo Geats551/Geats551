8f9c12c4216b52d23aa33091e33e52dd8be3aab7
ZZL-6 <2072174524@qq.com>
2024-12-05 13:02:14 +0800

    改

--- pythonProject8-8/myproject/myapp/admin.py
+++ pythonProject8-8/myproject/myapp/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import AgriculturalProduct
 from .models import Category
 from .models import Comment
+from .models import Purchase
 
 admin.site.register(Comment)
 admin.site.register(AgriculturalProduct)
 admin.site.register(Category)
+admin.site.register(Purchase)
